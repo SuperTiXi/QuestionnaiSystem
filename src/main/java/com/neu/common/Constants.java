318ee41c92f0653e24febf7e12d8206e815e27a4
SuperTiXi <20206641@stu.neu.edu.cn>
2022-11-02 11:39:53 +0800

    add redis

--- src/main/java/com/neu/common/Constants.java
+++ src/main/java/com/neu/common/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
      * Description:登录状态
      */
     public static final String LOGIN_SUCCESS_CODE = "666";
-    public static final String LOGIN_SUCCESS_MESSAGE = "登陆成功";
+    public static final String LOGIN_SUCCESS_MESSAGE = "登录成功";
     public static final String LOGIN_FAIL_CODE = "401";
     public static final String LOGIN_FAIL_MESSAGE = "登录失败";
     public static final String PHONE_ERROR = "手机号格式错误";
@@ -19,5 +19,14 @@ public class Constants {
     public static final String CODE_SEND_FAIL_CODE = "201";
     public static final String CODE_SEND_SUCCESS_MESSAGE = "验证码发送成功";
     public static final String CODE_SEND_FAIL_MESSAGE = "验证码发送失败";
+    public static final String CODE_DIE_MESSAGE = "验证码已过期";
+    public static final String CODE_ERROR_MESSAGE = "验证码错误";
+
+    /**
+     * Description:redis相关
+     */
+
+    public static final String LOGIN_CODE_KEY = "login:code";
+    public static final long LOGIN_CODE_TTL = 2L;
 }
 
--- src/main/java/com/neu/service/impl/AccountServiceImpl.java
+++ src/main/java/com/neu/service/impl/AccountServiceImpl.java
@@ -2,6 +2,7 @@ package com.neu.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.neu.bean.HttpResponseEntity;
 import com.neu.common.utils.HttpUtils;
@@ -10,17 +11,22 @@ import com.neu.dao.AccountMapper;
 import com.neu.dao.entity.Account;
 import com.neu.service.AccountService;
 import org.apache.http.HttpResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static com.neu.common.Constants.*;
 
 @Service
 public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> implements AccountService {
 
-    private String code;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public HttpResponseEntity loginByUserName(String userName, String password) {
@@ -43,7 +49,9 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
     @Override
     public HttpResponseEntity sendCode(String phone) {
         HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
-        code = RandomUtil.randomNumbers(6);
+        String code = RandomUtil.randomNumbers(6);
+
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
 
         log.error("验证码为"+code);
 
@@ -63,7 +71,7 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
         headers.put("Authorization", "APPCODE " + appcode);
         Map<String, String> querys = new HashMap<String, String>();
         querys.put("mobile", phone);
-        querys.put("param", "**code**:"+code+",**minute**:5");
+        querys.put("param", "**code**:"+code+",**minute**:2");
         querys.put("smsSignId", "2e65b1bb3d054466b82f0c9d125465e2");
         querys.put("templateId", "908e94ccf08b4476ba6c876d13f084ad");
         Map<String, String> bodys = new HashMap<String, String>();
@@ -107,15 +115,24 @@ public class AccountServiceImpl extends ServiceImpl<AccountMapper, Account> impl
             return httpResponseEntity;
         }
 
-        if(this.code.equals(code)){
-            httpResponseEntity.setCode(LOGIN_SUCCESS_CODE);
-            httpResponseEntity.setMessage(LOGIN_SUCCESS_MESSAGE);
-            httpResponseEntity.setData(account);
+        String redisCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
+
+        if(StrUtil.isEmpty(redisCode)){
+            httpResponseEntity.setCode(LOGIN_FAIL_CODE);
+            httpResponseEntity.setMessage(CODE_DIE_MESSAGE);
             return httpResponseEntity;
         }
 
-        httpResponseEntity.setCode(LOGIN_FAIL_CODE);
-        httpResponseEntity.setMessage(LOGIN_FAIL_MESSAGE);
+        if(!redisCode.equals(code)){
+            httpResponseEntity.setCode(LOGIN_FAIL_CODE);
+            httpResponseEntity.setMessage(CODE_ERROR_MESSAGE);
+
+            return httpResponseEntity;
+        }
+
+        httpResponseEntity.setCode(LOGIN_SUCCESS_CODE);
+        httpResponseEntity.setMessage(LOGIN_SUCCESS_MESSAGE);
+        httpResponseEntity.setData(account);
 
         return httpResponseEntity;
     }
